46f928dcb0346b9c84aa9298ac98bb9720e85d69
Isaiahchonchoramirez <Isaiahramirez37@gmail.com>
2025-04-28 12:42:06 -0400

    update vite / jason

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/isaiahramirezdev/',
+  base: '/isaiahramirezdev',
   plugins: [react(), tailwindcss()],
 });
