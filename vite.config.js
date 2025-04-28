110b21c25f945eca2f8b7c5cc698b09097a783f9
Isaiahchonchoramirez <Isaiahramirez37@gmail.com>
2025-04-28 12:26:11 -0400

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Github_pages/',
+  base: '/isaiahramirezdev/',
   plugins: [react(), tailwindcss()],
 });
